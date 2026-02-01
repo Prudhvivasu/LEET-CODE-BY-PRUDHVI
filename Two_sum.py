9b928efe79d32448725739e78aa4fbb92b178305
Prudhvivasu <kotiprudhvivasu@gmail.com>
2026-02-01 21:15:26 +0530

    Add files via upload

---
+++ Two_sum.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        l=len(nums)
+        for i in range(0,l):
+            for j in range(i+1,l):
+                if(nums[i]+nums[j]==target):
+                    return [i,j]
+
+        
+        
